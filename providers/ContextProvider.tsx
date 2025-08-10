a71da869ac448a8cbab704d26a8555e9d074db4b
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-08-10 20:21:25 +0600

    fix error

--- providers/ContextProvider.tsx
+++ providers/ContextProvider.tsx
@@ -1,15 +1,8 @@
 "use client"
-import { valid_theme } from '@/constant/variable'
-import { usePathname, useRouter } from 'next/navigation'
 import React, { createContext, useContext } from 'react'
 
 const globalContext = createContext(null)
 const ContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const pathName = usePathname()
-  const router = useRouter()
-  if (!valid_theme.includes(pathName?.split('/')[1])) {
-    return router.push('/light')
-  }
   return (
     <globalContext.Provider value={null}>
       {children}
