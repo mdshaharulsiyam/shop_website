6cf186607daad73a42d4cc93ee160d73dad491fa
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-08-10 08:03:13 +0600

    context providers

---
+++ constant/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  light: {
+    gray: "#E1E5E9",
+    black: "#000000",
+    white: "#FFFFFF",
+    red: "#FF0000",
+    green: "#00FF00",
+    blue: "#0000FF",
+    yellow: "#FFFF00",
+  },
+};
---
+++ providers/ContextProvider.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React, { createContext, useContext } from 'react'
+
+const globalContext = createContext(null)
+const ContextProvider = ({ children }: { children: React.ReactNode }) => {
+
+  return (
+    <globalContext.Provider value={null}>
+      {children}
+    </globalContext.Provider>
+  )
+}
+
+export default ContextProvider
+export const useGlobalContext = () => useContext(globalContext)
