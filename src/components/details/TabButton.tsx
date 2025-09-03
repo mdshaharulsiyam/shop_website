70ec56fb150047df9112b84ca760f541a4c8e2e7
siyam <siyam@shaharul.siyam>
2025-09-03 06:45:05 -0500

    tabs

--- src/components/details/TabButton.tsx
+++ src/components/details/TabButton.tsx
@@ -1,16 +1,19 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Specifications from './tabContent/Specifications'
 const TabButton = () => {
   return (
     <Tabs defaultValue="account" className="w-full my-4 mb-0 pb-4">
       <TabsList>
         <TabsTrigger value="descriptions">Descriptions</TabsTrigger>
         <TabsTrigger value="specifications">Specifications</TabsTrigger>
-        <TabsTrigger value="reviews">Reviews</TabsTrigger>
+        {/* <TabsTrigger value="reviews">Reviews</TabsTrigger> */}
         <TabsTrigger value="shop">Shop</TabsTrigger>
       </TabsList>
       <TabsContent value="descriptions">Make changes to your account here.</TabsContent>
-      <TabsContent value="specifications">Change your password here.</TabsContent>
-      <TabsContent value="reviews">View your reviews here.</TabsContent>
+      <TabsContent value="specifications">
+        <Specifications />
+      </TabsContent>
+      {/* <TabsContent value="reviews">View your reviews here.</TabsContent> */}
       <TabsContent value="shop">Browse our shop here.</TabsContent>
     </Tabs>
   )
---
+++ src/components/details/tabContent/Specifications.tsx
@@ -0,0 +1,14 @@
+
+const Specifications = () => {
+  return (
+    <ul className="mt-4 space-y-2 text-sm">
+      {/* {Object.entries(product_details).map(([key, value]) => (
+        <li key={key}>
+          <span className="font-semibold capitalize">{key.replace('_', ' ')}:</span> {value}
+        </li>
+      ))} */}
+    </ul>
+  )
+}
+
+export default Specifications
