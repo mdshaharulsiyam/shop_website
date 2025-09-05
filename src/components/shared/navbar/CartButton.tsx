1284b39202f931c649db9114af169915ff7e1306
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-09-05 12:28:23 +0600

    s

--- src/components/shared/navbar/CartButton.tsx
+++ src/components/shared/navbar/CartButton.tsx
@@ -1,8 +1,10 @@
 import BadgesButton from '@/components/buttons/BadgesButton';
+import IconButton from '@/components/buttons/IconButton';
 import CartCard from '@/components/cart/CartCard';
 import { useGlobalContext } from '@/providers/ContextProvider';
+import { hexToRGBA4, hexToRGBA6 } from '@/utils/hexToRGBA';
 import { Drawer } from 'antd';
-import { ShoppingCart } from 'lucide-react';
+import { ArrowBigLeft, ArrowBigRight, ShoppingCart } from 'lucide-react';
 import { useState } from 'react';
 const data = [
   {
@@ -52,6 +54,47 @@ const CartButton = () => {
         {
           data.map((item, i) => (<CartCard key={i} item={item} />))
         }
+        <div style={{
+          borderColor: hexToRGBA4(themeColor.gray)
+        }} className='mt-4 border-t px-4 py-3'>
+          <div className='flex items-center justify-between text-2xl'>
+            <p>Total</p>
+            <p>$299.95</p>
+          </div>
+          <div className='flex items-center justify-between text-base mt-3'>
+            <div style={{
+              color: themeColor.white
+            }} className='flex items-center justify-start text-base gap-2'>
+              <IconButton handler={() => console.log('Order Now')}
+                style={{
+                  backgroundColor: hexToRGBA6(themeColor.blue),
+                  padding: '4px 10px',
+                  borderRadius: '8px'
+                }}
+                icon={<ArrowBigLeft />} />
+              <IconButton handler={() => console.log('Order Now')}
+                style={{
+                  backgroundColor: hexToRGBA6(themeColor.blue),
+                  padding: '4px 10px',
+                  borderRadius: '8px'
+                }}
+                icon={<ArrowBigRight />}
+              />
+            </div>
+            <IconButton handler={() => console.log('Order Now')}
+              style={{
+                backgroundColor: hexToRGBA6(themeColor.green),
+                padding: '4px 10px',
+                borderRadius: '8px'
+              }}
+              icon={<div>
+                <p style={{
+                  color: themeColor.white
+                }}>Checkout</p>
+              </div>} />
+
+          </div>
+        </div>
       </Drawer>
     </>
   )
