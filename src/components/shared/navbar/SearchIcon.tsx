17dc47f05ea3fde25cc530de6065ce635253a2dd
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-09-05 15:36:57 +0600

    s

--- src/components/shared/navbar/SearchIcon.tsx
+++ src/components/shared/navbar/SearchIcon.tsx
@@ -1,13 +1,26 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Popover, PopoverTrigger } from '@/components/ui/popover'
+import { useGlobalContext } from '@/providers/ContextProvider'
+import { PopoverContent } from '@radix-ui/react-popover'
 import { Search } from 'lucide-react'
 
 const SearchIcon = () => {
+  const { themeColor } = useGlobalContext();
   return (
-    <div className='cursor-pointer'>
-      <Button variant="ghost" size="icon" className="hidden sm:flex">
-        <Search className="h-5 w-5" />
-      </Button>
-    </div>
+    <Popover>
+      <PopoverTrigger>
+        <Button variant="ghost" size="icon" className="hidden sm:flex cursor-pointer">
+          <Search className="h-5 w-5" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className=''>
+        <Input style={{
+          backgroundColor: themeColor.white,
+        }} type="text" placeholder="Search..." required />
+      </PopoverContent>
+    </Popover>
+
   )
 }
 
