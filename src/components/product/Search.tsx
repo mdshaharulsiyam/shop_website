87dc746f5e63b2547248d3e1d1d2d7868709757d
shaharulsiyam <shaharulsiyam0273@gmail.com>
2025-09-07 17:55:44 +0600

    s

--- src/components/product/Search.tsx
+++ src/components/product/Search.tsx
@@ -3,7 +3,7 @@ import { Input } from '../ui/input'
 
 const Search = () => {
   const { themeColor } = useGlobalContext()
-  return <Input className='mt-2 py-5 w-full' style={{
+  return <Input className='py-5 w-full' style={{
     backgroundColor: themeColor.white,
   }} type="text" placeholder="Search..." required />
 }
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,12 +1,21 @@
 import Search from '@/components/product/Search'
+import { useGlobalContext } from '@/providers/ContextProvider'
 import { Filter } from 'lucide-react'
 
 const Product = () => {
+  const { themeColor } = useGlobalContext()
   return (
     <div className=''>
-      <div className='flex justify-between items-center gap-3'>
+      <div className='flex justify-between items-center gap-3 mt-3'>
         <Search />
-        <Filter />
+        <button
+          style={{
+            backgroundColor: themeColor.white,
+
+          }}
+          className='p-2 rounded-md cursor-pointer'>
+          <Filter />
+        </button>
       </div>
       {/* <div className='lg:flex lg:flex-row justify-between items-start'>
         <div className='w-full lg:w-[70%]'>
