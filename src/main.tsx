29ced6d0f370b232b847e78b404a6abe2c56da8b
mdshaharulsiyam <shaharulsiyam56@gmail.com>
2025-10-05 00:19:33 -0700

    s

--- src/main.tsx
+++ src/main.tsx
@@ -20,3 +20,22 @@ createRoot(document.getElementById('root')!).render(
     </Provider>
   </StrictMode>,
 )
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCMtslSk_Dezxa6knjEMCSxKZYm2iQ39Zk",
+//   authDomain: "multivendor-shop-8c79d.firebaseapp.com",
+//   projectId: "multivendor-shop-8c79d",
+//   storageBucket: "multivendor-shop-8c79d.firebasestorage.app",
+//   messagingSenderId: "912381620809",
+//   appId: "1:912381620809:web:344b08f00d64386619c598"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
